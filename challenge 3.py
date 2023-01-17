4ce205a7b5d52348cc7d72b262bd6e947a626adf
prash1921 <120668760+prash1921@users.noreply.github.com>
2023-01-17 19:00:36 +0530

    Add files via upload

---
+++ challenge 3.py
@@ -0,0 +1,23 @@
+class student:
+ def __init__(self,name,rollno.):
+    self.studentname=name
+    self.rollno=rollno
+def set_name(self):
+    print("enter student name")
+    self.__studentname
+    pass
+def get_name(self):
+    print("enter student name")
+    return self.__studentname
+    pass
+def set_rollno(self):
+    print("enter rollno.")
+    return self.__rollno
+    pass
+
+def get_rollno(self):
+    print("enter rollno.")
+    return self.__rollno
+    pass
+print(studentname)
+print(rollno)
